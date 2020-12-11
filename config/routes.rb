eeffbdf456ccfc72541ba5f7db44fd90f906156a
banana38ruy <banana38ruy@yahoo.co.jp>
2020-12-11 14:25:54 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
   root to: "messages#index"
+   #作成するコントローラーにアクションを定義する
+   #基本的なアクションはCRUD
+   #railsではそれを７つに分ける
+   #resourcesはその７つが詰まったお得セット
+   #resources :必要とする住所 これだとそのまま７つ付与
+   #resources :必要とする住所, only: [:付与したいアクション, :付与したいアクション ]と記述する
+  resources :users, only: [:edit, :update]
 end
