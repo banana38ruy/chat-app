cfa6086705b6f1ccc87268dfcfa58f5f112a688d
banana38ruy <banana38ruy@yahoo.co.jp>
2020-12-13 18:18:10 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,8 +7,17 @@ class Message < ApplicationRecord
  # 一つのチャットルームに、メッセージは複数存在します
  # 一人のユーザーは、複数のメッセージを送信できる
  # チャットルームとユーザーの中間テーブルになるよここ
- validates :content, presence: true
- # からの場合はDBに保存させないために
+ validates :content, presence: true, unless: :was_attached?
+ # 空の場合はDBに保存させないために
  # validatesはモデルで設定する
+ # validatesのunlessオプションにメソッド名を指定することで
+ def was_attached?
+  self.image.attached?   
+ end
+ # メソッドの返り値がfalseならばバリデーションによる検証を行う
+ # という条件を作っている
+ # was_attached?メソッドは添付したの？ということで
+ # self.image.attached?自分の画像を添付したか？という記述によって
+ # 画像があればtrue,なければfalseを返す仕組み
 end
 
